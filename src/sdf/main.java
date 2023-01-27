54c8ccceb72b1f178f73799ff63953164504271a
weizhen94 <weizhen94@gmail.com>
2023-01-27 16:44:29 +0800

    final final

--- src/sdf/main.java
+++ src/sdf/main.java
@@ -1,40 +1,40 @@
 package sdf;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class main{
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
    
         FileReader reader = new FileReader("cat_in_the_hat.txt"); 
         Scanner scanner = new Scanner(reader); 
-        HashMap<String, Integer> wordCount = new HashMap<>(); 
+        Map<String, Integer> wordCount = new HashMap<>(); 
         
         while (scanner.hasNextLine()){
         String sentence = scanner.nextLine(); 
         
-
         String cleanSentence = sentence.replaceAll("\\p{P}", "");
         String upperCaseSentence = cleanSentence.toUpperCase(); 
         String[] splitWords = upperCaseSentence.split(" "); 
 
         for(String justWords:splitWords){
-            System.out.println(justWords);
+    
             if(!wordCount.containsKey(justWords)){
                 wordCount.put(justWords, 1); 
             } else {
                 wordCount.put(justWords, wordCount.get(justWords)+1);
             }
         }
-        ArrayList<HashMap<String, Integer>> arrayList = new ArrayList<HashMap<String, Integer>>();
-        arrayList.add(wordCount);
-
-        System.out.println(arrayList);
+        Stream<Map.Entry<String, Integer>> sortedWordCount = wordCount.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).limit(10); 
+        sortedWordCount.forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+        
         }
-
-    scanner.close(); 
+     scanner.close();
+     reader.close(); 
     }
 }
