2b8c86dd2d7e94da33bc605aa82eed4380b67aa2
weizhen94 <weizhen94@gmail.com>
2023-01-27 11:06:47 +0800

    3rd commit

--- src/sdf/main.java
+++ src/sdf/main.java
@@ -2,26 +2,29 @@ package sdf;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class main{
     public static void main(String[] args) throws FileNotFoundException {
-
-        double wordCount = 0; 
-
+   
         FileReader reader = new FileReader("cat_in_the_hat.txt"); 
         Scanner scanner = new Scanner(reader); 
-
+        Map<String, Integer> wordCount = new HashMap<>(); 
+        
+        while (scanner.hasNextLine()){
         String sentence = scanner.nextLine(); 
 
-        while (scanner.hasNextLine()){
-        if (sentence.equalsIgnoreCase ("mother")){
-            wordCount++; 
-            System.out.println("The frequency of the word is " + wordCount);
-        } 
-    }
+        String cleanSentence = sentence.replaceAll("\\p{P}", "");
+        
+        String[] splitWords = cleanSentence.split(" "); 
 
+        for(String justWords:splitWords){
+            System.out.println(justWords);
+        }
+        }
 
-        scanner.close(); 
+    scanner.close(); 
     }
 }
