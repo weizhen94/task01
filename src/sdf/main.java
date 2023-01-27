93a0558522987481aa51bcad8a7e1d1b000c8e73
weizhen94 <weizhen94@gmail.com>
2023-01-27 10:01:00 +0800

    3rd commit

--- src/sdf/main.java
+++ src/sdf/main.java
@@ -1,7 +1,27 @@
-package sdf; 
+package sdf;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class main{
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
+    public static void main(String[] args) throws FileNotFoundException {
+
+        double wordCount = 0; 
+
+        FileReader reader = new FileReader("cat_in_the_hat.txt"); 
+        Scanner scanner = new Scanner(reader); 
+
+        String sentence = scanner.nextLine(); 
+
+        while (scanner.hasNextLine()){
+        if (sentence.equalsIgnoreCase ("mother")){
+            wordCount++; 
+            System.out.println("The frequency of the word is " + wordCount);
+        } 
+    }
+
+
+        scanner.close(); 
     }
 }
